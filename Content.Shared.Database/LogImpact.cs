e271dac3656dcab12a67545b90b8261136abf116
Thomas <git@aeshus.com>
2025-01-14 09:19:47 -0600

    Attribute Content.Shared.Database

--- Content.Shared.Database/LogImpact.cs
+++ Content.Shared.Database/LogImpact.cs
@@ -1,4 +1,8 @@
-﻿namespace Content.Shared.Database;
+// SPDX-FileCopyrightText: 2017-2025 Space Wizards Federation and Contributors <https://github.com/space-wizards/space-station-14>
+//
+// SPDX-License-Identifier: LicenseRef-MIT-SpaceWizards
+
+namespace Content.Shared.Database;
 
 // DO NOT CHANGE THE NUMERIC VALUES OF THESE
 [Serializable]
