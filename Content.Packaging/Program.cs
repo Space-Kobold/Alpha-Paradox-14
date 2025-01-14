bca1a5962b7b602c437075337c32581da217b199
Thomas <git@aeshus.com>
2025-01-13 20:08:08 -0600

    Attribute Content.Packaging

--- Content.Packaging/Program.cs
+++ Content.Packaging/Program.cs
@@ -1,4 +1,8 @@
-﻿using Content.Packaging;
+// SPDX-FileCopyrightText: 2017-2025 Space Wizards Federation and Contributors <https://github.com/space-wizards/space-station-14>
+//
+// SPDX-License-Identifier: LicenseRef-MIT-SpaceWizards
+
+using Content.Packaging;
 using Robust.Packaging;
 
 IPackageLogger logger = new PackageLoggerConsole();
