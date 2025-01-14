52e80dfe6848f6f4b5b9c1f2c38b39abbb71f994
Thomas <git@aeshus.com>
2025-01-14 09:04:48 -0600

    Attribute Content.Tools

--- Content.Tools/test/run.sh
+++ Content.Tools/test/run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+# SPDX-FileCopyrightText: 2017-2025 Space Wizards Federation and Contributors <https://github.com/space-wizards/space-station-14>
+#
+# SPDX-License-Identifier: LicenseRef-MIT-SpaceWizards
+
 cp 0A.yml out.yml
 ../bin/Debug/net5.0/Content.Tools out.yml 0B.yml 0C.yml
