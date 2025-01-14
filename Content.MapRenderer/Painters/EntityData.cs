ffdfd46c1aff582200c88bed10cd5f1b04e994a7
Thomas <git@aeshus.com>
2025-01-13 20:06:52 -0600

    Attribute Content.MapRenderer

--- Content.MapRenderer/Painters/EntityData.cs
+++ Content.MapRenderer/Painters/EntityData.cs
@@ -1,4 +1,8 @@
-﻿using Robust.Client.GameObjects;
+// SPDX-FileCopyrightText: 2017-2025 Space Wizards Federation and Contributors <https://github.com/space-wizards/space-station-14>
+//
+// SPDX-License-Identifier: LicenseRef-MIT-SpaceWizards
+
+using Robust.Client.GameObjects;
 using Robust.Shared.GameObjects;
 
 namespace Content.MapRenderer.Painters;
