8844700027bc23c9a4cba6dba501ceff6507673b
Thomas <git@aeshus.com>
2025-01-14 09:05:40 -0600

    Attribute Content.Tests

--- Content.Tests/AssemblyInfo.cs
+++ Content.Tests/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using NUnit.Framework;
+// SPDX-FileCopyrightText: 2017-2025 Space Wizards Federation and Contributors <https://github.com/space-wizards/space-station-14>
+//
+// SPDX-License-Identifier: LicenseRef-MIT-SpaceWizards
+
+using NUnit.Framework;
 
 [assembly: Parallelizable(ParallelScope.Fixtures)]
